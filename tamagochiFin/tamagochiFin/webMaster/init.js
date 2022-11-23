881ab2401c6981fd8dc0588b1d04b81f4498892c
Carlosdb148 <92909837+Carlosdb148@users.noreply.github.com>
2022-11-23 21:20:22 +0100

    Add files via upload

---
+++ tamagochiFin/tamagochiFin/webMaster/init.js
@@ -0,0 +1,4 @@
+import { Master } from "./webMaster";
+
+let master = new Master();
+master.init();
---
+++ tamagochiFin/tamagochiFin/webMaster/object.js
@@ -0,0 +1,7 @@
+var tamagotchi = tamagotchi || {};
+tamagotchi.Object = class {
+    constructor(type){
+        this.type = type;
+        this.vida = 100;
+    }
+}

6d98c9c453925362281f19c5712f7e4bdf89eacc
Carlosdb148 <92909837+Carlosdb148@users.noreply.github.com>
2022-11-24 19:38:49 +0100

    Delete tamagochiFin/tamagochiFin directory

--- tamagochiFin/tamagochiFin/webMaster/init.js
+++
@@ -1,4 +0,0 @@
-import { Master } from "./webMaster";
-
-let master = new Master();
-master.init();
--- tamagochiFin/tamagochiFin/webMaster/object.js
+++
@@ -1,7 +0,0 @@
-var tamagotchi = tamagotchi || {};
-tamagotchi.Object = class {
-    constructor(type){
-        this.type = type;
-        this.vida = 100;
-    }
-}

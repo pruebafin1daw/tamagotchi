bf2e3bd9ed8b91b6472562f627d4d020d350c3d6
aurbano145 <80650797+aurbano145@users.noreply.github.com>
2022-11-30 10:49:08 +0100

    Add files via upload

--- js/client.js
+++ js/client.js
@@ -1,41 +1,18 @@
-"use strict";
+import {Communication} from "./communication.js";
 
-let socket = new WebSocket("ws://localhost:8023");
-//socket.binaryType = "arraybuffer";
-
-let messageText = document.getElementById("message");
-
-socket.onopen = function(e) {
-    alert("[open] Conexión establecida");
-    alert("Enviando al servidor");
-    /*const msg = {
-        "tipo" : 1,
-        "mensaje" : "Hello World",
+class Client {
+    active = false;
+    map = null;
+    
+    init(comunication) {
+        this.comunication = comunication;
+        this.comunication.handler = this;
+        this.comunication.send("nuevo",Communication.MASTER);
     }
-    socket.send(JSON.stringify(msg));*/
-    socket.send("Hello Server");
-};
 
-socket.onmessage = function(event) {
-    console.log(event);
-
-    alert(`[message] Datos recibidos del servidor: ${event.data}`);
-
-    let text = event.data;
-
-    messageText.innerHTML = text;
-};
-
-socket.onclose = function(event) {
-    if (event.wasClean) {
-        alert(`[close] Conexión cerrada limpiamente, código=${event.code} motivo=${event.reason}`);
-    } else {
-        // ej. El proceso del servidor se detuvo o la red está caída
-        // event.code es usualmente 1006 en este caso
-        alert('[close] La conexión se cayó');
+    newMsg(msg,origin) {
+        console.log(msg);
     }
-};
+}
 
-socket.onerror = function(error) {
-    alert(`[error] ${error.message}`);
-};
+export {Client};

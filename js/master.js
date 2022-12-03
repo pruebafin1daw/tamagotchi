aa77ef8cf95a9cff23be840ac884f92c8addc242
cmaroto307 <109908350+cmaroto307@users.noreply.github.com>
2022-12-03 16:46:29 +0100

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,14 +1,15 @@
 class Master {
         
-    init(config,communication) {
+    init(config, communication) {
+        this.config = config;
         this.communication = communication;
         this.communication.handler = this;
         this.players = [];
         this.map = new Array();
         this.edges = [];
-        for (let i=0;i<config.height;i++) {
+        for (let i=0;i<this.config.height;i++) {
             this.map[i] = new Array();
-            for (let j=0;j<config.width;j++) {
+            for (let j=0;j<this.config.width;j++) {
                 this.map[i][j] = {
                     y: i,
                     x: j,
@@ -17,19 +18,19 @@ class Master {
                     burrow: false,
                     burrowPlayer: null
                 }
-                if ((i==0) || (i==config.height-1) || (j==0) || (j==config.width-1)) {
+                if ((i==0) || (i==this.config.height-1) || (j==0) || (j==this.config.width-1)) {
                     if ((i+j)%2) {
                         this.map[i][j].burrow = true;
                         this.edges.push(this.map[i][j]);
                     }
-                } else if(Math.random() < config.porcentage){
+                } else if(Math.random() < this.config.porcentage){
                     this.map[i][j].burrow = true;
                 }
             }
         }
         let clientMap = { 
-            width : config.width,
-            height : config.height,
+            width : this.config.width,
+            height : this.config.height,
             burrow : this.map.flatMap(num => num).filter(item => item.burrow).map(item => {
                 return {
                     y : item.y,
@@ -68,7 +69,7 @@ class Master {
         }
     }
 
-    manageShift() { // Método que debe de ser lanzdo al comienzo de la partida con una promesa cada X tiempo
+    manageShift() { // Método que debe de ser lanzado al comienzo de la partida con una promesa cada X tiempo
         this.manageBattles();
         this.restoreLife();
         this.killPlayer();
@@ -78,7 +79,7 @@ class Master {
         this.players(i => {
             this.players(j => {
                 if(i.x == j.x && i.y == j.y) {
-                    j.energy -= 10; // Valor pasado por config?
+                    j.energy -= this.config.lifeTakenDamage;
                 }
             });
         });
@@ -87,10 +88,10 @@ class Master {
     restoreLife() {
         this.players(player => {
             if(player.inBurrow) {
-                player.energy += 10; // Valor pasado por config?
+                player.energy += this.config.lifeRestoredBurrow;
             }
             else if (!this.players.find(i => i.x == player.x && i.y == player.y)) {
-                player.energy += 2; // Valor pasado por config?
+                player.energy += this.config.lifeRestoredAlone;
             }
         });
     }
@@ -102,6 +103,10 @@ class Master {
                 this.players.slice(this.players.indexOf(player), 1);
             }
         });
+    }
+
+    endgame() {
+        
     }
 }
 
